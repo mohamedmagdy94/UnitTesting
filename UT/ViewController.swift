0961f03b2445916119a0822f51e2ec759773b4d9
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2020-09-21 14:11:36 +0200

    Initial Project

---
+++ UT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UT
+//
+//  Created by Mohamed El-Taweel on 9/21/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
